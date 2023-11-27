3953f97eb08eebb9ca6d8730ff49b8353b0edb2b
AditKhurana <85402636+AditKhurana14@users.noreply.github.com>
2023-11-27 17:52:11 +0530

    fixed production build

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h2>BroShop</h2>
+                <h2>BroShop <sub style={{fontSize: '13px'}}>- 'Bro' is a friendly remark!</sub></h2>
             </div>
         </div>
     )
